f5a411564268e332be326833434f11044cefabbd
bresc19 <Brescia.teo@gmail.com>
2021-04-20 18:08:00 +0200

    added vuejs template

---
+++ vue-cli-ex/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <home-page v-if="currentPage === 'home'" />
+    <post-list-page v-if="currentPage === 'posts'" />
+ </div>
+</template>
+
+<script>
+
+import HomePage from './pages/HomePage.vue';
+import PostListPage from "./pages/PostListPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomePage,
+    PostListPage,
+  },
+  data(){
+    return {
+      currentPage: "home"
+    }
+  },
+  mounted(){
+    if(window.location.pathname === "/"){
+      this.currentPage = "home"
+    } else if(window.location.pathname === "/posts"){
+      this.currentPage = "posts"
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

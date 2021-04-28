750be022e1a0566fa68b4966643f450d570f2e72
bresc19 <Brescia.teo@gmail.com>
2021-04-28 19:02:31 +0200

    added footer and header to the layout

---
+++ project/components/Footer.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="col-container">
+
+    <div class="col">
+      <h2>TopTech</h2>
+      <a class="footer-link" id="footer-left">© 2021 TopTech S.p.A. <br>
+        Via Schiaffino 11
+        20158 Milano, Italy <br>
+        P. IVA IT13187610152</a>
+
+    </div>
+
+    <div class="col">
+      <h2>Explore</h2>
+      <a class="footer-link" href="../HomePage.html">Home</a>
+      <a class="footer-link" href="Products.html">Products</a>
+      <a class="footer-link" href="People.html"> People </a>
+      <a class="footer-link" href="AboutUs.html">About Us</a>
+      <a class="footer-link" href="Contact.html">Contact</a>
+    </div>
+
+    <div class="col">
+      <h2>Areas</h2>
+      <a class="footer-link" href="../../HomePage.html">Data Bases</a>
+      <a class="footer-link" href="Products.html">IoT</a>
+      <a class="footer-link" href="People.html">Security</a>
+      <a class="footer-link" href="AboutUs.html">Machine Learning</a>
+    </div>
+
+    <div class="col">
+      <h2>Social</h2>
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-instagram"></a>
+      <a href="#" class="fa fa-linkedin"></a>
+      <a href="#" class="fa fa-twitter"></a>
+    </div>
+
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
+@import "../assets/css/style.css";
+* {
+  box-sizing: border-box;
+}
+
+.col-container {
+  position: relative;
+  display: table;
+  width: 100%;
+  bottom: 0;
+  border-top:1px solid #00F0FF;
+  horiz-align: center;
+}
+
+
+.col {
+  display: table-cell;
+  flex: 1;
+  padding: 16px;
+  background: #7F828B;
+  horiz-align: center;
+}
+
+@media only screen and (max-width: 600px) {
+  .col {
+    display: inline-block;
+    width: 100%;
+  }
+}
+
+
+/* Style all font awesome icons */
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
+
+}
+
+/* Add a hover effect if you want */
+.fa:hover {
+  opacity: 0.7;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+
+a.footer-link{
+  display: block;
+  text-decoration: none;
+  outline: none;
+  color: black;
+}
+
+#footer-left{
+  color: #adc9ff;
+}
+
+
+</style>

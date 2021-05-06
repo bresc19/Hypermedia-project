814f10ce44f3a87bbc2c3a038db7b5b514880f79
bresc19 <Brescia.teo@gmail.com>
2021-05-06 10:41:00 +0200

    update configuration

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -200,7 +200,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: app.router.options.base,
-      env: {"BASE_URL":"localhost:3000","DATABASE_URL":"postgres://lnjtjbzjogupnb:3347350d1c81eaa341f905fc7e436ef53053e45c73aa4542a00dc0f225692086@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/d5kfvb6ra7u04b"}
+      env: {"BASE_URL":"https://toptech-polimi.herokuapp.com","DATABASE_URL":"postgres://lnjtjbzjogupnb:3347350d1c81eaa341f905fc7e436ef53053e45c73aa4542a00dc0f225692086@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/d5kfvb6ra7u04b"}
     }
     // Only set once
 

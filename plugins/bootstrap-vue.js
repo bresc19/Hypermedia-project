53506fc65b8330e72e1c2b6df879b2de52e0262b
bresc19 <Brescia.teo@gmail.com>
2021-05-10 19:10:23 +0200

    Update bootstrap-vue.js

--- plugins/bootstrap-vue.js
+++ plugins/bootstrap-vue.js
@@ -3,9 +3,11 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { DropdownPlugin } from 'bootstrap-vue'
+import { CarouselPlugin } from 'bootstrap-vue'
 
 
 
 
+Vue.use(CarouselPlugin)
 Vue.use(DropdownPlugin)
 Vue.use(BootstrapVue)

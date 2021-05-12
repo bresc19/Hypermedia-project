4643d5d2ff4c566c67ecc6bbf8c7a39963cd64b2
bresc19 <Brescia.teo@gmail.com>
2021-05-12 15:47:10 +0200

    Update Areas.vue

--- pages/Areas.vue
+++ pages/Areas.vue
@@ -1,196 +1,114 @@
 <template>
-  <!--
-  <div>
-    <div id="container-carousel">
-      <div>
-      <b-carousel
-          id="carousel-1"
-          v-model="slide"
-          :interval="4000"
-          controls
-          indicators
-          style="display: inline-block"
-          @sliding-start="onSlideStart"
-          @sliding-end="onSlideEnd"
-      >
-        <b-carousel-slide
-            text="Whether you're looking for compute power, database storage, content delivery, or other functionality, TopTech has the services to help you build sophisticated applications with increased flexibility, scalability and reliability"
-            :img-src="require('~/assets/img/areas11.jpg')">
-        </b-carousel-slide>
+<div>
 
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-          <h1>Hello world!</h1>
-        </b-carousel-slide>
-
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-
-        <b-carousel-slide>
-          <template #img>
-            <img
-                class="d-block img-fluid w-100"
-                width="1024"
-                height="480"
-                src="https://picsum.photos/1024/480/?image=55"
-                alt="image slot"
-            >
-          </template>
-        </b-carousel-slide>
-
-        <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-            a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-          </p>
-        </b-carousel-slide>
-      </b-carousel>
+  <div id="carouselExampleIndicators" class="carousel slide container-carousel" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="~/assets/img/areas11.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <p>Whether you're looking for compute power, database storage, content delivery, or other functionality, TopTech has the services to help you build sophisticated applications with increased flexibility, scalability and reliability.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="~/assets/img/areas11.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="~/assets/img/areas11.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+<div id="cont-area-link">
+
+  <div class="card card-area"  style="width: 18rem;">
+    <img class="card-img-top" src="~/assets/img/Database/db-logo.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Data Base</h5>
+      <p class="card-text">With Toptech databases, you don’t need to worry about database management tasks such as server provisioning, patching, configuration, or backups.</p>
+      <a href="#" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+        <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+      </svg></a>
     </div>
+  </div>
 
-
-
-    <div id="cont-area-link">
-      <b-card
-          title="Data Base"
-          :img-src="require('~/assets/img/Database/db-logo.png')"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 200px;
-                max-height: 600px;"
-          class="mb-2 card-area"
-      >
-        <b-card-text>
-          Toptech databases, you don’t need to worry about database management tasks such as server provisioning, patching, configuration, or backups.
-        </b-card-text>
-
-        <b-button class="link-area" href="#" variant="primary">
-          <b-icon-arrow-up rotate="90"></b-icon-arrow-up>
-        </b-button>
-      </b-card>
-
-
-      <b-card
-          title="IoT"
-          :img-src="require('~/assets/img/IoT/IOT.png')"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 200px;
-                max-height: 600px;"
-          class="mb-2"
-      >
-        <b-card-text>
-          With the proliferation of devices, you increasingly need solutions to connect them, and collect, store, and analyze device data.        </b-card-text>
-
-        <b-button class="link-area" href="#" variant="primary">
-          <b-icon-arrow-up rotate="90"></b-icon-arrow-up>
-        </b-button>
-      </b-card>
-
-
-
-      <b-card
-          title="Security"
-          :img-src="require('~/assets/img/Security/security-logo.png')"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 200px;
-                max-height: 600px;"
-          class="mb-2"
-      >
-        <b-card-text>
-          Security concerns dominate how we outsource computation. Emerging security technology will fundamentally change future IT systems.
-        </b-card-text>
-
-        <b-button class="link-area" href="#" variant="primary">
-          <b-icon-arrow-up rotate="90"></b-icon-arrow-up>
-        </b-button>
-      </b-card>
-
-
-
-      <b-card
-          title="Machine Learning"
-          :img-src="require('~/assets/img/MachineLearning/ml-logo.png')"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 200px;
-                max-height: 600px;"
-          class="mb-2"
-      >
-        <b-card-text>
-          Explore machine learning services that fit your business needs, and learn how to get started.
-        </b-card-text>
-
-        <b-button class="link-area" href="#" variant="primary">
-          <b-icon-arrow-up rotate="90"></b-icon-arrow-up>
-        </b-button>
-      </b-card>
-
-
+  <div class="card"  style="width: 18rem;">
+    <img class="card-img-top" src="~/assets/img/IoT/IOT.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">IoT</h5>
+      <p class="card-text">With the proliferation of devices, you increasingly need solutions to connect them, and collect, store, and analyze device data.</p>
+      <a href="#" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+        <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+      </svg></a>
     </div>
+  </div>
 
+
+  <div class="card"  style="width: 18rem;">
+    <img class="card-img-top" src="~/assets/img/Security/security-logo.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Security</h5>
+      <p class="card-text">Security concerns dominate how we outsource computation. Emerging security technology will fundamentally change future IT systems.</p>
+      <a href="#" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+        <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+      </svg></a>
+    </div>
+  </div>
+
+
+  <div class="card"  style="width: 18rem;">
+    <img class="card-img-top" src="~/assets/img/MachineLearning/ml-logo.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Machine Learning</h5>
+      <p class="card-text">Explore machine learning services that fit your business needs, and learn how to get started.</p>
+      <a href="#" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+        <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+      </svg></a>
+    </div>
+  </div>
+</div>
 
 
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
-        <div class="flex-container">
-          <div>
-            <img class="area-logo-image" src="../assets/img/Database/db-logo.png">
-            <button class="btn-1 title-area-areas">Data Base</button>
-            <p class="description-area-areas">With
-              Toptech databases, you don’t need to worry about database management tasks such as server provisioning, patching, configuration, or backups.
-            </p>
-          </div>
-          <div>
-            <img class="area-logo-image" src="../assets/img/IoT/IOT.png">
-            <button class="btn-1 title-area-areas">IoT</button>
-            <p class="description-area-areas">With the proliferation of devices, you increasingly need solutions to connect them, and collect, store, and analyze device data.</p>
-          </div>
-          <div>
-            <img class="area-logo-image" src="../assets/img/Security/security-logo.png">
-            <button class="btn-1 title-area-areas">Security</button>
-            <p class="description-area-areas">Security concerns dominate how we outsource computation. Emerging security technology will fundamentally change future IT systems.</p>
-          </div>
-          <div>
-            <img class="area-logo-image" src="../assets/img/MachineLearning/ml-logo.png">
-            <button class="btn-1 title-area-areas">Machine Learning</button>
-            <p class="description-area-areas">Explore machine learning services that fit your business needs, and learn how to get started.</p>
-          </div>
-        </div>
-      </div>
--->
   </template>
   <script>
 
-  import Vue from 'vue'
-
-  import { CarouselPlugin } from 'bootstrap-vue'
 
 
   export default {
     name: 'Areas',
     layout: 'default',
+    components: [
+    ]
 
   }
 
 
   </script>
   <style scoped>
+  @font-face
   .flex-container {
     margin: inherit;
     horiz-align: center;
@@ -266,7 +184,7 @@
     width: auto;
     position: relative;
   }
-  #container-carousel{
+  .container-carousel{
     margin: 0 auto;
     width: 50%;
     height: 50%;
@@ -293,4 +211,12 @@
     bottom: 5px;
     position: absolute;
   }
+
+
+
+
+
+
+
+
   </style>

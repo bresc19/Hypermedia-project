b0611c1487f53e929979735fee0fc7a7f8e14045
bresc19 <Brescia.teo@gmail.com>
2021-05-12 10:00:50 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     }
   ],
   ssr: true,
-  env:{
+  env: {
     //BASE_URL: 'https://toptech-polimi.herokuapp.com',
     //BASE_URL: 'https://localhost:3000',
     //DATABASE_URL: 'postgres://postgres:admin@localhost:5432/hypermedia-project'
@@ -19,47 +19,62 @@ export default {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+      },
+
+    ],
+
+    script: [
+
+      {
+        src: "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+        type: "text/javascript"
+      },
+      {
+        src:
+            "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js",
+        type: "text/javascript"
+      },
+      {
+        src:
+            "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+        type: "text/javascript"
+      },
+    ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/bootstrap.min.css'
-    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+// Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
+// https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
+// Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/axios',
 
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-
-  },
+// Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
 
   router: {
     base: '/'
   },
-  axios:{
- }
+  axios: {}
 }

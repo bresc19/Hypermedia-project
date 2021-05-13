6c51bdef4bbea2171d9a5693528a3753dfaacf69
bresc19 <Brescia.teo@gmail.com>
2021-05-13 10:21:17 +0200

    configuration eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,24 @@ export default {
         href: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
       },
 
+      {
+        rel: "stylesheet",
+        href: "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      },
+
+      {
+        rel: "stylesheet",
+      href: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      },
+      {
+        rel: "stylesheet",
+        src: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      },
+      {
+        rel: "stylesheet",
+        src: "./assets/css/fontawesome.min.css"
+      }
+
     ],
 
     script: [

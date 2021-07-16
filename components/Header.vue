21a0b7cebdfef4038e4d6be5b3adfd7c8bc91d65
bresc19 <Brescia.teo@gmail.com>
2021-07-16 18:49:36 +0200

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container-header">
+  <header class="container-header">
     <nav class="container container-menu">
       <div class="row align-self-md-center no-gutters" >
         <div class="col-sm container-menu-item " >
           <a class="align-self-md-center" href="/">
-            <img style="  transform: translate(0, 50%);"  src="~/assets/img/toptech_logo.png" width="135" height="30">
+            <img style="  transform: translate(0, 60%);"  src="~/assets/img/toptech_logo.png" width="135" height="30">
           </a>
         </div>
         <div class="col-sm">
@@ -46,7 +46,7 @@
       </div>
     </nav>
 
-  </div>
+  </header>
 </template>
 
 <script>
@@ -70,14 +70,21 @@ export default {
 .container-header {
   background-color: #0071CE;
   border: 1px solid black;
-  height: 60px;
+  height: 80px;
+  width: 100%;
 }
+@media screen and (max-width: 600px) {
+  .container-header {
+    height: 100%;
+  }
+}
+
 
 .menu-item{
   vertical-align: middle;
   text-decoration: none;
   color: white;
-  transform: translate(0, 32%);
+  transform: translate(0, 50%);
 }
 
 .menu-item:hover{
